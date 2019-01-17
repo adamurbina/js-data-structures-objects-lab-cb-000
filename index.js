0aa9b3bc023476da9b8b808f327085f7fbc16349
Adam Urbina <adamurbina@gmail.com>
2019-01-17 00:40:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const driver = {};
 
-function updateDriverWithKeyAndValue(driver, key, value) {
-  newDriver = {...driver, [key] = value};
+function updateDriverWithKeyAndValue (driver, key, value) {
+  const newDriver = {...driver, [key] = value};
   return newDriver;
 }
